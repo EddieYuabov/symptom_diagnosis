b101fb4cac510006d2211441c29b358bc2a5be63
Edward Yuabov <yuaboved@gmail.com>
2022-12-12 10:02:38 -0500

    Got rid of default vue code

--- symptom-diagnosis-app/src/App.vue
+++ symptom-diagnosis-app/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <h1 id="header">Website</h1>
+    </header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  color: #80cbc4;
+  -webkit-text-stroke: .3px rgb(0, 0, 0);
+  font-size: 30px;
 }
 </style>
