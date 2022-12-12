ff7def5261c0be4d069fcaa20cb89e2b2a6e1382
Edward Yuabov <yuaboved@gmail.com>
2022-12-12 14:27:33 -0500

    created delete functionality for diagnosis model backend

--- symptom-diagnosis-backend/controllers/DiagnosisController.js
+++ symptom-diagnosis-backend/controllers/DiagnosisController.js
@@ -16,8 +16,20 @@ const CreateDiagnosis = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
+const UpdateDiagnosis = async (req, res) => {
+  try {
+    const diagnosis = await User_diagnosis.update(
+      { ...req.body },
+      { where: { id: req.params.diagnosis_id }, returning: true }
+    )
+    res.send(diagnosis)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 module.exports = {
   GetDiagnosis,
-  CreateDiagnosis
+  CreateDiagnosis,
+  UpdateDiagnosis
 }
--- symptom-diagnosis-backend/routers/DiagnosisRouter.js
+++ symptom-diagnosis-backend/routers/DiagnosisRouter.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/DiagnosisController')
 
 router.get('/', controller.GetDiagnosis)
 router.post('/new', controller.CreateDiagnosis)
+router.put('/:diagnosis_id', controller.UpdateDiagnosis)
 
 module.exports = router
