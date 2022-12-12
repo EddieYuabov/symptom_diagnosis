698417e1fee7a855e00c43640efcce142a90a95d
Edward Yuabov <yuaboved@gmail.com>
2022-12-12 14:24:45 -0500

    created update functionality for diagnosis model backend

--- symptom-diagnosis-backend/controllers/DiagnosisController.js
+++ symptom-diagnosis-backend/controllers/DiagnosisController.js
@@ -8,7 +8,16 @@ const GetDiagnosis = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
+const CreateDiagnosis = async (req, res) => {
+  try {
+    const diagnosis = await User_diagnosis.create({ ...req.body })
+    res.send(diagnosis)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 module.exports = {
-  GetDiagnosis
+  GetDiagnosis,
+  CreateDiagnosis
 }
--- symptom-diagnosis-backend/routers/DiagnosisRouter.js
+++ symptom-diagnosis-backend/routers/DiagnosisRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const controller = require('../controllers/DiagnosisController')
 
 router.get('/', controller.GetDiagnosis)
+router.post('/new', controller.CreateDiagnosis)
 
 module.exports = router
