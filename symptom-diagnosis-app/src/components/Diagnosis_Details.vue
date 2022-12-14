2bc45a0dded0c140796993dd50ca34a76b7e516d
Edward Yuabov <yuaboved@gmail.com>
2022-12-14 15:05:13 -0500

    created functionality to get id of diagnosis from params

--- symptom-diagnosis-app/src/components/Diagnosis_Details.vue
+++ symptom-diagnosis-app/src/components/Diagnosis_Details.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
-        <h1>Diagnosis Details</h1>
+        <div>
+        <h1>{{diagnoses.name}}</h1>
+        <h3>{{diagnoses.possible_cause}}</h3>
+        <h4>{{diagnoses.treatment}}</h4>
+        </div>
+
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+const URL = 'http://localhost:3001/home'
 export default {
-        name: 'DiagnosisDetails'
+        name: 'DiagnosisDetails',
+        data: () => ({
+            diagnoses: []
+        }),
+        mounted: function(){
+            this.getDiagnosis()
+        },
+        methods: {
+            async getDiagnosis(){
+                const response = await axios.get(`${URL}/diagnosis/${this.$route.params.diagnosis_id}`)
+                this.diagnoses = response.data
+                console.log(this.diagnoses)
+            }
+        }
 }
 </script>
 
