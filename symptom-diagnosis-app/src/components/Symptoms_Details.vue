912d212c2384f14d6a694ce5a4b0fabc58ae6fb3
Edward Yuabov <yuaboved@gmail.com>
2022-12-14 15:33:35 -0500

    did functionality for symptoms page

--- symptom-diagnosis-app/src/components/Symptoms_Details.vue
+++ symptom-diagnosis-app/src/components/Symptoms_Details.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        <h1>Symptoms Details</h1>
+        <h1>{{symptoms.symptom}}</h1>
+        <button id="edit-button">&#9998;</button>
+        <button id="delete-button" @click="deleteSymptom">&#128465;</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+const URL = 'http://localhost:3001/home'
 export default {
-        name: 'SymptomsDetails'
+        name: 'SymptomsDetails',
+        data: () => ({
+            symptoms: []
+        }),
+        mounted: function(){
+            this.getSymptom()
+        },
+        methods: {
+            async getSymptom(){
+                const response = await axios.get(`${URL}/symptoms/${this.$route.params.symptom_id}`)
+                this.symptoms = response.data
+            },
+            async deleteSymptom(e){
+                e.preventDefault()
+                await axios.delete(`${URL}/symptoms/${this.$route.params.symptom_id}`)
+                this.$router.push(`/symptoms`)
+            }
+        }
 }
 </script>
 
